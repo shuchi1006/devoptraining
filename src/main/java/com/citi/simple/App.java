7549ccec9acda212dab036ea4ee16537570a553c
Shuchi <goyshuchi@gmail.com>
2022-05-18 12:31:59 +0530

    Third Commit

--- src/main/java/com/citi/simple/App.java
+++ src/main/java/com/citi/simple/App.java
@@ -4,7 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("******** Next App***********************************");
+		System.out.println("******** Sample App-2 **********");
 	}
 
 }
